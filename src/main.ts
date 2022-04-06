d9707a136eb14c67ed671fd0191c5f3b698a1480
Carolina Xavier <joelestumano@gmail.com>
2022-04-06 16:38:20 -0300

    update port

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { GlobalExceptionHandling } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
   app.useGlobalFilters(new GlobalExceptionHandling());
-  // await app.listen(3000);
-  await app.listen(process.env.PORT || 8080);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
